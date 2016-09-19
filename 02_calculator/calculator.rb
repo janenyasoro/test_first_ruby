6cd69d6d9156db6c0a97a082260ec769de2cb3cf
janenyasoro <janenyasoro@gmail.com>
2016-09-19 19:03:34 +0300

    how to calculate the sum, multiplication,addition, subtraction and power of numbers

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,20 @@
+def add(c,d)
+	c + d
+end
+def subtract(e,f)
+	e - f
+	end
+def sum(j = [x])
+sum = 0
+j.inject(0){|sum, x|sum+ x.to_i}
+end
+def multiply(a,b)
+	a * b
+end
+def multiplies(k,l)
+	k * l 
+	
+end
+def power(f,g)
+	f ** g
+end
